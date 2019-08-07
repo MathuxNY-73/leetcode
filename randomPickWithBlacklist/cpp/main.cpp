01cd3700f18ad5d2f3c0d388c7a8000462ed090b
MathuxNY-73 <pouillaude.a@gmail.com>
2019-08-07 23:59:13 +0200

    Adds header file for input functions

--- randomPickWithBlacklist/cpp/main.cpp
+++ randomPickWithBlacklist/cpp/main.cpp
@@ -23,6 +23,8 @@
 #include <unordered_set>
 #include <vector>
 
+#include "input.hpp"
+
 #define wl(n) while(n--)
 #define min(a,b) a < b ? a : b
 #define max(a,b) a < b ? b : a
@@ -77,60 +79,6 @@ public:
     }
 
 };
-
-static inline void fastscan(int& number)
-{
-    //variable to indicate sign of input number
-    bool negative = false;
-    int c;
-
-    number = 0;
-
-    // extract current character from buffer
-    c = getchar_unlocked();
-
-    if (c=='-')
-    {
-        // number is negative
-        negative = true;
-
-        // extract the next character from the buffer
-        c = getchar_unlocked();
-    }
-
-    // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0'(48) to '9' (57)
-    for (; (c>47 && c<58); c=getchar_unlocked())
-        number = number *10 + c - 48;
-
-    // if scanned input has a negative sign, negate the
-    // value of the input number
-    if (negative)
-        number *= -1;
-}
-
-static inline int fastscan_string_w(char (&str)[], int buffer_size)
-{
-    char c;
-    int size_of_str = 0;
-
-    // extract current character from buffer
-    c = getchar_unlocked();
-
-    // Keep on extracting characters if they are not
-    // space, carriage return or end of string
-    for (; c != ' '
-             && c != '\n'
-             && c != '\0'
-             && c != EOF;
-         c=getchar_unlocked(),++size_of_str)
-        {
-            if(size_of_str > buffer_size)
-                exit(-1);
-            str[size_of_str] = c;
-        }
-    return  size_of_str;
-}
 
 int main()
 {
