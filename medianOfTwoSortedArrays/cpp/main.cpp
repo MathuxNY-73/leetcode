1428baef0ea9b8b3d3f7dd1eeb383ee2fb338652
MathuxNY-73 <pouillaude.a@gmail.com>
2019-08-14 11:48:46 +0200

    Refactors CPP code of Median Of Two Sorted arrays

--- medianOfTwoSortedArrays/cpp/main.cpp
+++ medianOfTwoSortedArrays/cpp/main.cpp
@@ -1,30 +1,30 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <vector> 
-#include <list>
-#include <queue>
-#include <set>
-#include <map>
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <functional>
-#include <numeric>
-#include <cstdlib>
-#include <cmath>
-#include <cstdio>
-#include <fstream>
-#include <ctime>
 #include <cassert>
-#include <unordered_set>
-#include <unordered_map>
-#include <functional>
-#include <stack>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <algorithm>
 #include <array>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "input.hpp"
+
 
 #define wl(n) while(n--)
 #define fl(i,a,b) for(i=a; i<b; ++i)
@@ -120,36 +120,6 @@ public:
         return res;
     }
 };
-
-inline void fastscan(int& number)
-{
-    //variable to indicate sign of input number
-    bool negative = false;
-    int c;
- 
-    number = 0;
- 
-    // extract current character from buffer
-    c = getchar_unlocked();
-    if (c=='-')
-    {
-        // number is negative
-        negative = true;
- 
-        // extract the next character from the buffer
-        c = getchar_unlocked();
-    }
- 
-    // Keep on extracting characters if they are integers
-    // i.e ASCII Value lies from '0'(48) to '9' (57)
-    for (; (c>47 && c<58); c=getchar_unlocked())
-        number = number *10 + c - 48;
- 
-    // if scanned input has a negative sign, negate the
-    // value of the input number
-    if (negative)
-        number *= -1;
-}
 
 int main()
 {
