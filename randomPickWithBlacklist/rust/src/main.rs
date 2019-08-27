81f909ba88b2b5455e84fb76cee39c6d8eea08f2
MathuxNY-73 <pouillaude.a@gmail.com>
2019-08-28 01:17:25 +0200

    Adds input to rust code in random pick pb

--- randomPickWithBlacklist/rust/src/main.rs
+++ randomPickWithBlacklist/rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::collections::HashMap;
 use rand::distributions::Uniform;
 use rand::Rng;
@@ -50,10 +51,30 @@ impl Solution {
 }
 
 fn main() {
-    let res = Solution::new(4, vec![0,1,3]);
-    let pick = res.pick();
-    println!("Res is {}", pick);
+    use myio::Scanner;
+
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+
+    let n = scan.token::<usize>();
+
+    for _ in 0..n {
+        let N = scan.token::<i32>();
+        let b_size = scan.token::<usize>();
+
+        let blacklist = (0..b_size).map( |_| scan.token::<i32>()).collect();
+
+        let sol = Solution::new(N, blacklist);
+
+        let n_pick = scan.token::<i32>();
+
+        for _ in 0..n_pick {
+            print!("{} ", sol.pick());
+        }
+        println!("");
+    }
 }
+
 
 
 
