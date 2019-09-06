ea9b0651b9b91c0062ff7998c765aa4feeebe2e2
MathuxNY-73 <pouillaude.a@gmail.com>
2019-09-06 23:53:50 +0200

    BruteForced the Shortest Subarray problem, wrong.

--- medianOfTwoSortedArrays/rust/main.rs
+++ medianOfTwoSortedArrays/rust/main.rs
@@ -78,6 +78,7 @@ impl Solution {
 }
 
 fn main() {
+    
     let res = Solution::find_median_sorted_arrays(vec![0,1,3], vec![3,4,5,5]);
     println!("Res is {}", res);
 }
--- shortestSubarrayWithSumAtLeastK/main.cpp
+++ shortestSubarrayWithSumAtLeastK/main.cpp
@@ -23,96 +23,65 @@
 #include <unordered_set>
 #include <vector>
 
-#include "input.hpp"
-
 #define wl(n) while(n--)
-#define min(a,b) a < b ? a : b
-#define max(a,b) a < b ? b : a
 #define fl(i,a,b) for(i=a; i<b; ++i)
 
 using namespace std;
 
+static const auto _____ = []()
+                          {
+                              ios::sync_with_stdio(false);
+                              cin.tie(nullptr);
+                              cout.tie(nullptr);
+                              return nullptr;
+                          }();
+
 class Solution {
-private:
-    mt19937 gen;
-    uniform_int_distribution<int> dis;
-    std::map<int,int> m;
-    int bound;
 public:
-    Solution(int N, vector<int>& blacklist): gen(mt19937(random_device()())), dis(uniform_int_distribution<int>(0,N - 1 - blacklist.size())), bound(N-blacklist.size()) {
-        sort(blacklist.begin(), blacklist.end());
-        m = std::map<int, int>();
-        int k = 0;
-        while(k < blacklist.size() && blacklist[k] < bound) ++k;
 
-        auto whitelist = vector<int>(k, 0);
-        for(int i = bound, j = k, l = 0; i < N && l < k ; ++i, ++l) {
-            while(j < blacklist.size() && blacklist[j] == i) ++i,++j;
-            whitelist[l] = i;
+    static int shortestSubarray(const vector<int>& A, int K) {
+        auto a_length = A.size();
+        auto min_length = a_length;
+        auto found = false;
+
+        for(auto i = 0 ; i < a_length ; ++i) {
+            auto cum = 0;
+            for(auto j = i ; j < min(i + min_length, a_length) ; ++j) {
+                if(A[j] >= K) {
+                    return 1;
+                }
+                else {
+                    cum += A[j];
+                    if(cum >= K) {
+                        found = true;
+                        min_length = j - i + 1;
+                        //cout << "min_length: " << min_length << "\n";
+                        break;
+                    }
+                }
+            }
         }
-
-        printf("Whitelist: ");
-        for(auto& w: whitelist) {
-            printf("%d ", w);
-        }
-        printf("\n");
-
-        for(int i = 0 ; i < k ; ++i) {
-            m[blacklist[i]] = whitelist[i];
-        }
-
-        printf("Map: ");
-        for(auto&& [f,s]: m) {
-            printf("%d -> %d ", f,s);
-        }
-        printf("\n");
-
-        srand(time(NULL));
+        return found ? min_length : -1;
     }
-
-    int pick() {
-        int picked = dis(gen);
-        //while (picked >= bound) {
-        //    picked = rand() / ((RAND_MAX + 1u) / bound);
-        //}
-        return m.find(picked) != m.cend() ? m[picked] : picked;
-    }
-
 };
 
 int main()
 {
     int t;
-    fastscan(t);
-
+    cin >> t;
     wl(t)
     {
-        int n=0;
-        fastscan(n);
-        int nb_blacklist = 0;
-        fastscan(nb_blacklist);
-        auto blacklist = vector<int>(nb_blacklist, 0);
+        int n=0, k=0;
+        cin >> n >> k;
+        auto array = vector<int>(n, 0);
 
         int i;
-        fl(i, 0, nb_blacklist){
-            fastscan(blacklist[i]);
+        fl(i, 0, n){
+            cin >> array[i];
         }
 
-        auto rand_gen = Solution(n, blacklist);
-
-        int nb_pick = 0;
-        fastscan(nb_pick);
-
-        printf("N = %d, nb_blacklist: %d, nb_pick: %d\n", n, nb_blacklist, nb_pick);
-        for(auto& b: blacklist) {
-            printf("%d ", b);
-        }
-        printf("\n");
-        wl(nb_pick) {
-            auto res = rand_gen.pick();
-            printf("%d ", res);
-        }
-        puts("");
+        auto res = Solution::shortestSubarray(array, k);
+        cout << res << "\n";
     }
 
     return 0;
