a8e19f36c21ef11e58dce1539f7bb68fbc44b6ee
MathuxNY-73 <pouillaude.a@gmail.com>
2019-09-29 23:43:35 +0200

    Starts solving the findNumberOfLIS problem

---
+++ findNumberOfLIS/main.cpp
@@ -0,0 +1,98 @@
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "input.hpp"
+
+#define wl(n) while(n--)
+#define fl(i,a,b) for(i=a; i<b; ++i)
+
+using namespace std;
+
+class Solution {
+public:
+    static int findNumberOfLIS(const vector<int>& nums) {
+        auto n_size = nums.size();
+        auto dp = vector<pair<int,int>>(n_size, {0,0});
+
+        for(int i = 0 ; i < n_size ; ++i) {
+            auto max_item = 0;
+            auto cnt = 1;
+            for(int j = 0 ; j < i ; ++j) {
+                if(nums[j] < nums[i]) {
+                    if(dp[j].first >  max_item) {
+                        max_item = dp[j].first;
+                        cnt = 1;
+                    }
+                    else if (dp[j].first == max_item) {
+                        ++cnt;
+                    }
+                }
+            }
+            dp[i] = {max_item + 1, cnt};
+        }
+
+        for(auto& p: dp) {
+            printf("(%d %d) ", p.first, p.second);
+        }
+        printf("\n");
+
+        auto max_item = 0;
+        for(int i = 0 ; i < n_size ; ++i) {
+            if(dp[i].first > max_item) {
+                max_item = dp[i].first;
+            }
+        }
+
+        auto count = 0;
+        for(int i = 0 ; i < n_size ; ++i) {
+            if(dp[i].first == max_item) {
+                count += dp[i].second;
+            }
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    int t;
+    fastscan(t);
+
+    wl(t)
+    {
+        int n=0;
+        fastscan(n);
+
+        auto nums = vector<int>(n, 0);
+        for(int i = 0 ; i < n ; ++i) {
+            fastscan(nums[i]);
+        }
+        auto res = Solution::findNumberOfLIS(nums);
+        printf("%d\n", res);
+    }
+
+    return 0;
+}
