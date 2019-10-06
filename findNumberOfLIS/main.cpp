0fad96972e5585ec578406e0583d6ac156907b2d
MathuxNY-73 <pouillaude.a@gmail.com>
2019-10-06 23:59:50 +0200

    Continue solving Number of LIS

--- findNumberOfLIS/main.cpp
+++ findNumberOfLIS/main.cpp
@@ -32,44 +32,65 @@ using namespace std;
 
 class Solution {
 private:
-    vector<int> tree;
+    pair<int, int> merge(const pair<int, int>& a, const pair<int, int>& b) const {
+        if(a.first == b.first) {
+            if(a.first == 0) {
+                return {0, 1};
+            }
+            return {a.first, a.second + b.second};
+        }
+        return a.first > b.first ? a : b;
+    }
+
+    void insert(vector<pair<int, int>>& tree, pair<int, int> val, int key, int idx, int lo, int hi) const {
+        if(lo == hi) {
+            tree[idx] = merge(val, tree[idx]);
+            return;
+        }
+
+        int mid = (lo + hi) / 2;
+
+        if(key <= mid) {
+            insert(tree, val, key, 2*idx + 1, lo, mid);
+        }
+        else {
+            insert(tree, val, key, 2 * (idx + 1), mid + 1, hi);
+        }
+
+        tree[idx] = merge(tree[2*idx + 1], tree[2*(idx+1)]);
+    }
+
+    pair<int, int> query(const vector<pair<int, int>>& tree, int key, int idx, int lo, int hi) {
+        auto mid = (lo + hi) / 2;
+
+        if(key >= hi) {
+            return tree[idx];
+        }
+        else if (key < lo) {
+            return {0, 1};
+        }
+        else {
+            return merge(query(tree, key, 2*idx + 1, lo, mid), query(tree, key, 2*(idx+1), mid + 1, hi));
+        }
+    }
 public:
     int findNumberOfLIS(const vector<int>& nums) {
         auto n_size = nums.size();
-        auto dp = vector<pair<int,int>>(n_size, {0,0});
 
-        for(int i = 0 ; i < n_size ; ++i) {
-            auto max_item = 0;
-            auto cnt = 1;
-            for(int j = 0 ; j < i ; ++j) {
-                if(nums[j] < nums[i]) {
-                    if(dp[j].first >  max_item) {
-                        max_item = dp[j].first;
-                        cnt = dp[j].second;
-                    }
-                     else if (dp[j].first == max_item) {
-                        cnt += dp[j].second;
-                    }
-                }
-            }
-            dp[i] = {max_item + 1, cnt};
+        if(n_size == 0) {
+            return 0;
         }
 
-        for(auto& p: dp) {
-            printf("(%d %d) ", p.first, p.second);
-        }
-        printf("\n");
+        auto tree = vector<pair<int, int>>(4 * n_size, {0,1});
 
-        auto max_item = max_element(dp.cbegin(), dp.cend(), [](const pair<int,int>& l, const pair<int, int>& r) -> bool {return l.first < r.first;});
-
-        auto count = 0;
-        for(int i = 0 ; i < n_size ; ++i) {
-            if(dp[i].first == max_item->first) {
-                count += dp[i].second;
-            }
+        auto max_el = *(max_element(nums.cbegin(), nums.cend()));
+        auto min_el = *(min_element(nums.cbegin(), nums.cend()));
+        for(auto& el : nums) {
+            auto q = query(tree, el - 1, 0, min_el, max_el);
+            insert(tree, {q.first + 1, q.second}, el, 0, min_el, max_el);
         }
 
-        return count;
+        return tree[0].second;
     }
 };
 
