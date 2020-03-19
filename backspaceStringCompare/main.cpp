b79dedbfca026620222d1df05275303acfa4a123
MathuxNY-73 <pouillaude.a@gmail.com>
2020-03-19 23:50:29 +0100

    Starts solving the backspace string compare

---
+++ backspaceStringCompare/main.cpp
@@ -0,0 +1,70 @@
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+#include <algorithm>
+#include <array>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "input.hpp"
+
+#define wl(n) while(n--)
+#define fl(i,a,b) for(i=a; i<b; ++i)
+
+using namespace std;
+
+static int fast_stream = []() {
+                             ios::sync_with_stdio(false);
+                             cin.tie(nullptr);
+                             cout.tie(nullptr);
+                             return 0;
+                         }();
+
+class Solution {
+public:
+    bool backspaceCompare(string S, string T) {
+
+    }
+};
+
+int main()
+{
+    int t;
+    fastscan(t);
+
+    wl(t)
+    {
+        int n=0;
+        fastscan(n);
+
+        auto nums = vector<int>(n, 0);
+        for(int i = 0 ; i < n ; ++i) {
+            fastscan(nums[i]);
+        }
+        auto res = Solution().plusOneB(nums);
+        for(auto& d: res) {
+            printf("%d", d);
+        }
+        printf("\n");
+
+    }
+
+    return 0;
+}
